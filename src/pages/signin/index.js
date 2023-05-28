95215d8fda1a59ca75e63d26b80378c5335eac21
yraffic <yraffic@gmail.com>
2023-05-28 16:59:20 -0300

    feat: page signin

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -2,8 +2,21 @@ import './styles.css';
 
 export const  Signin = ()=>{
   return (
-    <div>
-      <h1>oi</h1>
+    <div className='container'>
+      <div className='card-signin flex flex-col'>
+        <h1>Welcome to CodeLeap network!</h1>
+
+        <label for='user'>
+        Please enter your username
+        </label>
+
+        <input type='text' id='user' className='input'/>
+        <div className='card-bottom-btn flex'>
+          <button className='btn primary'>
+            ENTER
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
